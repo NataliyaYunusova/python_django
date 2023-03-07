79e9ae35ac4d39f92d332aa9bc790f8949c06352
Наталья Юнусова <yunusova.nv@gmail.com>
2023-03-07 15:34:24 +0300

    The homework is done

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,18 @@
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+        ('Electric stove', 570),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, "shopapp/shop-index.html", context=context)
