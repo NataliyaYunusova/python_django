eeae6831bf19d07b9e72ddba01b887f21ff7e595
Наталья Юнусова <yunusova.nv@gmail.com>
2023-04-05 09:50:02 +0300

    The homework_8 is done

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -11,7 +11,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myauth/', include('myauth.urls')),
     path('shop/', include('shopapp.urls')),
     path('req/', include('requestdataapp.urls')),
-
 ]
